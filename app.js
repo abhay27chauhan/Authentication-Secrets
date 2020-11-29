059272a63b575f5257eea90a28434433e87d9190
Abhay <chauhan27.abhay@gmail.com>
2020-11-29 20:23:44 +0530

    changes in package.json

--- app.js
+++ app.js
@@ -170,8 +170,5 @@ app.post("/login", function (req, res) {
     })
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+const port = process.env.PORT || 3000;
 app.listen(port);
